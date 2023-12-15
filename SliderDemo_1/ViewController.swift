86f873a40e59252e2afcece968ebd44af02260be
Ravshanbek Tursunbaev <rtursunbaev9822@gmail.com>
2023-12-15 10:59:04 +0500

    Initial Commit

---
+++ SliderDemo_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SliderDemo_1
+//
+//  Created by Ravshan Winter on 15/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
